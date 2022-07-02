8c14d5652e030cfa1aca7f7eefa77b22458ae435
lukasobg <lukas.olenborg@aalto.fi>
2022-07-02 15:03:35 +0300

    Improved toy_problem.jl

--- src/toy_problem.jl
+++ src/toy_problem.jl
@@ -18,19 +18,42 @@ s = 12 # n of suppliers
 toy_ins = create_toy_instance(s)
 
 # Initialize models
-m0_lin = Model(Gurobi.Optimizer) # NMDT - deterministic
 m0_nlm = Model(Gurobi.Optimizer) # nonlinear
 
 # Create the models with the problem data from toy_ins
-m_lin = create_linear_model(m0_lin, toy_ins, 3)
 m_nlm = create_nonlinear_model(m0_nlm, toy_ins)
 
 # Optimize the models with the problem data from toy_ins
-m_lin_opt = run_linear_model(m_lin) 
 m_nlm_opt = run_nonlinear_model(m_nlm) 
+
+# Results
+println("---------------- NONLINEAR -----------------")
+res_nlm = get_results(m_nlm_opt,s,0,"nlm")
+
+res = []
+push!(res, res_nlm)
+
+res_file = string("../toy_problem/comparison.csv")
+open(res_file, "a") do f
+     write(f, "\n\n")
+     write(f, "sup;cap;risk;PFAD;AF;CO;total biom;prod biod;demand;quality;prod cost;trans cost;process cost;hydro cost;total cost\n")
+     writedlm(f, res, ';')
+end
+
+println("Biomasses before blending:")
+println(value.(m_nlm_opt[:y]))
+
+#=
+# Initialize models
+m0_lin = Model(Gurobi.Optimizer) # NMDT - deterministic
+
+# Create the models with the problem data from toy_ins
+m_lin = create_linear_model(m0_lin, toy_ins, 4)
+
+# Optimize the models with the problem data from toy_ins
+m_lin_opt = run_linear_model(m_lin) 
 
 # Results
 println("---------------- LINEAR NMDT -----------------")
 res_lin = get_results(m_lin_opt,s,0,"lin-nmdt")
-println("---------------- NONLINEAR -----------------")
-res_nlm = get_results(m_nlm_opt,s,0,"nlm")
+=#
